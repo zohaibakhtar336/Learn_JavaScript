77098d82afb271346ee7db583e77fc68029a1182
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2025-02-16 17:37:06 +0530

    prototype in js

---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,37 @@
+const user = {
+    username: "Zohaib",
+    loginCount: 7,
+    signedIn: true,
+
+    getUserDetails: function(){
+        // console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        // console.log(this);
+    }
+
+}
+
+
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+const userOne = new User("Zohaib", 7, true)
+const userTwo = new User("Learn JavaScript", 11, false)
+console.log(userOne);
+console.log(userTwo);
